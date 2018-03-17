a8e562d3bc87de56d7e09ab0f3f88517bd100f28
Lisa Wang <rodent129@icloud.com>
2018-03-17 21:41:41 +0800

    Implement add category features.

--- Todoey/Controllers/CategoryListViewController.swift
+++ Todoey/Controllers/CategoryListViewController.swift
@@ -10,19 +10,81 @@ import UIKit
 import CoreData
 
 class CategoryListViewController: UITableViewController {
+    
+    var categoryArray = [Category]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        loadCategoryItems()
     }
     
     //MARK: - tableView DataSource
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categoryArray.count
+    }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCellItem", for: indexPath)
+        
+        let categoryItem = categoryArray[indexPath.row]
+        
+        cell.textLabel?.text = categoryItem.name
+        
+        return cell
+    }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
     
     //MARK: - Data Handle methods
     
     //MARK: - action handle methods
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add New Category", message: nil, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add", style: .default) { (alertAction) in
+            let textFields = alert.textFields
+            if let count = textFields?.count, count > 0 {
+                let newCategory = Category(context: self.context)
+                newCategory.name = textFields![0].text!
+                self.categoryArray.append(newCategory)
+                self.saveCategoryItems()
+                self.tableView.reloadData()
+            }
+        }
+        
+        
+        alert.addTextField { (textField) in
+            textField.placeholder = "Create a category"
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func loadCategoryItems() {
+        let request: NSFetchRequest<Category> = Category.fetchRequest()
+        
+        do {
+            categoryArray = try context.fetch(request)
+        }
+        catch {
+            print("Load category item errors: \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func saveCategoryItems() {
+        do {
+            try context.save()
+        }
+        catch {
+            print("Save category item errors: \(error)")
+        }
     }
 }
